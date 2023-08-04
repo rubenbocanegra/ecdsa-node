de8b02dae05cb454dd1fee475a47abf851972b9f
Rubén Bocanegra <rb@aconsoft.com>
2023-08-03 22:46:20 -0500

    Corrections in crypto library invocation2

--- server/index.js
+++ server/index.js
@@ -7,9 +7,9 @@ app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "0493d4252af1d924fe74c2a0d6a825a2eb6d488099279a2360154db6508f4acf00a71b185d319be32c6503538d9b0f661d0f8ebfac9199adf9e464bca70e03bea6": 100,
+  "04e35a613cd6b793535904de1a7ffbafc4b24a88bbb7d6e4d98f2e07aca28a37581d36fdece36c7b8ca65733a4b69507586899e5572a82da61c0b832c6ad2d2b54": 50,
+  "04a25d7ad5bc259202c3d7010d5bc5dd9dc55f8003483646dd1530da1e8be412e49ffd3018496f0d2c756f9537a41960bcbc7a1016b4bb62e5aee008fd519b0ac4": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
---
+++ server/scripts/generate.js
@@ -0,0 +1,8 @@
+const secp  = require('ethereum-cryptography/secp256k1');
+const {toHex} = require("ethereum-cryptography/utils");
+
+const privateKey =  secp.secp256k1.utils.randomPrivateKey();
+const publicKey = secp.secp256k1.getPublicKey(privateKey,false);
+
+console.log(toHex(privateKey))
+console.log(toHex(publicKey))
